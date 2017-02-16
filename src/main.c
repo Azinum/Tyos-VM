b052a01464da0281104d99bb98e879c06cfc7e91
Azinum <lukaska61@gmail.com>
2017-02-16 17:03:40 +0100

    Recursion test

--- src/main.c
+++ src/main.c
@@ -5,31 +5,18 @@
 
 #include "vm.h"
 
-/*
-def, (int)id, (int)size,
- data0,
- data1, ...
- ret
-*/
 
 int main(int argc, const char* argv[]) {
 	char code[] = {
 		I_DEF,
 	      1, 0, 0, 0,	/* ID */
-		  12, 0, 0, 0,	/* SIZE */
+		  17, 0, 0, 0,	/* SIZE */
 		  I_PUSH_INT, 9, 00, 00, 00,
 		  I_PUSH_INT, 8, 00, 00, 00,
 		  I_ADD,
-		  I_RET,
-		I_DEF,
-	      2, 0, 0, 0,
-		  12, 0, 0, 0,
-		  I_PUSH_INT, 3, 00, 00, 00,
-		  I_PUSH_INT, 4, 00, 00, 00,
-		  I_MULT,
+		  I_CALL, 0x1, 00, 00, 00,
 		  I_RET,
 		I_CALL, 0x1, 00, 00, 00,
-		I_CALL, 0x2, 00, 00, 00,
 		I_EXIT,
 	};
 	
